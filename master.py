47b9cd1fefa35275f4855bafe9ef48f549979345
barbara lopes <lopes.barbarav@gmail.com>
2025-06-02 22:35:18 +0000

    Evoluindo...

--- master.py
+++ master.py
@@ -6,7 +6,13 @@ st.title("Bem-vindo(a) ao Chat MoreTalk")
 with st.chat_message("user"):
     st.write("Hello Human!")
 
-a1 = st.chat_input("Say something..")
-if a1 == "Olá":
-    st.write("Olá! Como posso te ajudar hoje? :)")
-    
+agradecimentos = ["obrigado", "valeu", "agradeço", "muito obrigada"]
+mensagem = st.chat_input()
+
+def resposta(agradecimentos,mensagem):
+    for palavra in agradecimentos:
+        if palavra in mensagem:
+            return ("Um das palavras está contida na mensagem!")
+
+    return ("Nenhuma palavra está contida na mensagem!")
+
