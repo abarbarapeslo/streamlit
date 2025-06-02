085915bb1d2086a8e727f5d259638e89be80fa3f
barbara lopes <lopes.barbarav@gmail.com>
2025-06-02 22:41:22 +0000

    Primeira conversa

--- master.py
+++ master.py
@@ -10,9 +10,14 @@ agradecimentos = ["obrigado", "valeu", "agradeço", "muito obrigada"]
 mensagem = st.chat_input()
 
 def resposta(agradecimentos,mensagem):
+    mensagem = mensagem.lower()
     for palavra in agradecimentos:
         if palavra in mensagem:
             return ("Um das palavras está contida na mensagem!")
 
     return ("Nenhuma palavra está contida na mensagem!")
+
+if mensagem: #Só executa se o usuário digitou algo
+    with st.chat_message("bot"): #mensagem formatada para o usuário...
+        st.write(resposta(agradecimentos, mensagem)) #A mensagem entregue ao usuário
 
